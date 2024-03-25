b4164dd4d56b7aea3c930f28af01382353f8fd93
Matias Roces <matias.roces.web@gmail.com>
2024-03-25 19:56:07 -0300

    Agregado validación de los productos

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
+// Product Manager Class creates a manager of products
 class ProductManager {
     constructor() {
         this.products = []
     }
     addProduct(productName, productDescription, productPrice, productThumbnail, productCode, productStock = 0) {
-        const productId = this.products.length + 1
+        const validProduct = productName && productDescription && productPrice && productThumbnail && productCode
+        console.log(validProduct)
+        if (!validProduct) {
+            console.error(`Error: Some of data products is missing, please fill all the data of the new product`)
+            return
+        }
         const codeFinder = this.products.find((product) => product.code === productCode)
         if (codeFinder) {
             console.error(`The code ${productCode} is already in use. Please select another code for this product. `)
             return
         }
+
+        const productId = this.products.length + 1
         const producto = {
             id: productId,
             title: productName,
@@ -31,12 +39,12 @@ class ProductManager {
             console.error("Not found.")
             return
         }
-        console.log(productFinder)
         console.log(`The product with the id number ${productId} is ${productFinder.title}`)
     }
 }
 // Program function testing
 const productManager = new ProductManager()
+productManager.addProduct("Banana", "From Ecuador", 400, "22", 70)
 productManager.addProduct("Banana", "From Ecuador", 400, "this is a route", "22", 70)
 productManager.addProduct("Apple", "Gaucho", 700, "this is a route", "22", 50)
 productManager.addProduct("Apple", "Gaucho", 700, "this is a route", "23", 50)
